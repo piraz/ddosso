230f4ec46f0d8324f7a122e26ae0c34bd82034ba
Flavio Garcia <piraz@candango.org>
2016-09-12 01:26:18 -0400

    Added app config file.
    Refs: #1

--- ddosso/app.py
+++ ddosso/app.py
@@ -7,4 +7,8 @@ class DDOSSOComponent(firenado.tornadoweb.TornadoComponent):
     def get_handlers(self):
         return [
             (r'/', handlers.IndexHandler),
+            (r'/sso_login', handlers.IndexHandler),
         ]
+
+    def get_config_file(self):
+        return "ddosso"
